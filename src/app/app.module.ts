86b22c8b14812d7cc7a336436b460e8b89b72fdd
Edinson Morales <e.moralesm2@uniandes.edu.co>
2022-05-01 22:45:19 -0500

    creacion del modulo vehiculos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { VehiculosModule } from './vehiculos/vehiculos.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VehiculosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
